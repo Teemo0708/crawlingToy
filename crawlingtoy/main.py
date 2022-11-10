d99221d833ee4eebcf6a177ec397f0e5e5a24f55
양명호 <105221225+Teemo0708@users.noreply.github.com>
2022-11-10 22:22:34 +0900

    json형식으로 결과 변환

--- crawlingtoy/main.py
+++ crawlingtoy/main.py
@@ -1,4 +1,9 @@
+# -- coding: utf-8 --
 from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+
 
 app = FastAPI()
 
@@ -9,7 +14,32 @@ async def root():
 
 @app.get("/test/{input}")
 async def crawling(input):
-    return {"message": "Hello World"}
+    
+    try:
+        
+        page = requests.get("https://library.gabia.com/")
+        
+        soup = bs(page.content, 'html.parser',from_encoding='utf-8')
+        
+        elements = soup.select('div.esg-entry-content a.eg-grant-element-0')
+        list_ele = list(elements)
+        
+        res_dict= {}
+
+        for index, elements in enumerate(list_ele):
+            
+            res_dict[index] = elements.text, elements.attrs['href']
+            
+        
+        
+        json_val = json.dumps(res_dict, ensure_ascii=False) # ensure_ascii=False 붙여넣기
+        
+        print(json_val)
+        
+        return json_val
+    
+    except Exception as e :
+        print(e)
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
