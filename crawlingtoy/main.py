36a01945633d7e8b37ccc85989bb145a153ba8bf
양명호 <105221225+Teemo0708@users.noreply.github.com>
2022-11-15 17:33:40 +0900

    네이버 쇼핑인사이트 api사용

--- crawlingtoy/main.py
+++ crawlingtoy/main.py
@@ -3,6 +3,8 @@ from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup as bs
 import json
+import pprint
+
 
 
 app = FastAPI()
@@ -29,8 +31,6 @@ async def crawling(input):
         for index, elements in enumerate(list_ele):
             
             res_dict[index] = elements.text, elements.attrs['href']
-            
-        
         
         json_val = json.dumps(res_dict, ensure_ascii=False) # ensure_ascii=False 붙여넣기
         
@@ -40,6 +40,43 @@ async def crawling(input):
     
     except Exception as e :
         print(e)
+        
+@app.get("/items/select/{select_cat}")
+async def selectC(select_cat):
+
+    ####################################
+    client_id = "SOmSP6p7sz6n7sSYB2GZ"
+    client_secret = "asqHDSqHK9"
+    ####################################
+
+    url = "https://openapi.naver.com/v1/datalab/shopping/categories"
+
+    payload = {"startDate": "2022-10-01",
+            "endDate": "2022-11-13",
+            "timeUnit": "month",
+            "category": [
+                {"name": "식품",
+                    "param": ["50000006"]},
+                {"name": "건강식품",
+                    "param": ["50000023"]}],
+            "device": "pc",
+            #"ages": [],
+            #"gender": 
+                }
+
+    headers = {"X-Naver-Client-Id": client_id, "X-Naver-Client-Secret": client_secret, "Content-Type": "application/json"}
+    res = requests.post(url, data=json.dumps(payload), headers=headers)
+
+    print(res.status_code)
+    aaa = json.loads(res.text)
+
+    json_val2 = json.dumps(aaa, ensure_ascii=False)
+
+    pprint.pprint(json_val2)
+    #print(type(aaa))
+    
+    return json_val2
+    
 
 @app.get("/items/{item_id}")
 async def read_item(item_id):
